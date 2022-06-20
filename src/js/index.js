3b84407ca6b41accd4441c6e70a15411874a5141
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-20 14:11:02 -0300

    Finalizando o consumo da api

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+
+const cep = document.querySelector("#cep");
+const showData = (resultado)=>{
+    for(const campos in resultado) {
+        if(document.querySelector("#"+campos)){
+            document.querySelector("#"+campos).value = resultado[campos];
+            console.log(campos);
+        }
+    }
+}
+cep.addEventListener("blur",(event) => {
+
+    let busca = cep.value.replace("-", "");
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${busca}/json/`, options)
+    .then(response =>{ response.json()
+        .then( data => showData(data))
+    })
+    .catch(event => console.log('ERROR: ' + event,message));
+});
